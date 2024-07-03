110de596de59b83890d92053dbb983661c5e9749
vovanshil95 <vovanshil95@mail.ru>
2024-07-03 12:09:54 +0300

    add catchup

--- dags/get_vacancies.py
+++ dags/get_vacancies.py
@@ -263,7 +263,8 @@ with DAG(
     default_args=default_args,
     description='receives vacancies from hh and writes them to the database',
     start_date=datetime(2024, 6, 30),
-    schedule_interval='@daily'
+    schedule_interval='@daily',
+    catchup=False
 ) as dag:
     task1 = PythonOperator(
         task_id='get_currency_rates',
